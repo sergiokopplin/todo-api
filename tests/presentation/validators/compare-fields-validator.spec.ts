381afa2fffd61c11ec9b882c45684666ebc54f2d
Sergio Kopplin <sergiokopplin@gmail.com>
2021-02-20 19:41:22 -0300

    feat: ensure CompareFieldsValidator returns empty if validation is ok

--- tests/presentation/validators/compare-fields-validator.spec.ts
+++ tests/presentation/validators/compare-fields-validator.spec.ts
@@ -5,9 +5,26 @@ import { InvalidParamError } from '@/presentation/errors'
 
 const field = faker.internet.email()
 
+interface SutTypes {
+  sut: CompareFieldsValidator
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidator('field', 'fieldToCompare')
+
+  return {
+    sut
+  }
+}
+
 describe('RequiredFieldValidator', () => {
   test('Should return an error if validation fails', () => {
     const sut = new CompareFieldsValidator('field', 'fieldToCompare')
     expect(sut.validate({ field: field, fieldToCompare: 'invalid' })).toEqual(new InvalidParamError('field'))
+  })
+
+  test('Should return empty validation is ok', () => {
+    const { sut } = makeSut()
+    expect(sut.validate({ field: field, fieldToCompare: field })).toBeFalsy()
   })
 })
