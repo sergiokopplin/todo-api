dfff989e22f2533f5266eac46cd1fd1379e80f91
Sergio Kopplin <sergiokopplin@gmail.com>
2021-02-20 16:18:12 -0300

    refactor: updates ServerError message

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,7 +1,7 @@
 export class ServerError extends Error {
   constructor (stack: string) {
     super('Internal Server Error')
-    this.name = 'Something Unexpected Happen'
+    this.name = 'ServerError'
     this.stack = stack
   }
 }
