555619f92c42ff6c2b272ab354fc3470dbbcd85d
Sergio Kopplin <sergiokopplin@gmail.com>
2021-02-24 08:03:03 -0300

    feat: adds MongoHelper

---
+++ src/infra/db/mongodb/mongo-helper.ts
@@ -0,0 +1,26 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  uri: null as string,
+  client: null as MongoClient,
+
+  async connect(uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+    this.client = null
+  },
+
+  async getCollection(collection: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
+    return this.client.db().collection(collection)
+  }
+}
---
+++ tests/infra/db/mongodb/mongo-helper.spec.ts
@@ -0,0 +1,19 @@
+import { MongoHelper as sut } from '@/infra/db'
+
+describe('MongoHelper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should reconnect is mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
