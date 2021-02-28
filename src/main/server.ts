a60f2ef27b18c821a907f3d8131b0336c282414f
Sergio Kopplin <sergiokopplin@gmail.com>
2021-02-27 23:05:29 -0300

    feat: adds Mongo connect on server start

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,13 @@
 import 'module-alias/register'
 import { app, env } from '@/main/config'
+import { MongoHelper } from '@/infra/db'
 
-app.listen(env.port, () => {
-  console.log(
-    `⚡️ [server]: Server is running at https://localhost:${env.port}`
-  )
-})
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(
+        `⚡️ [server]: Server is running at https://localhost:${env.port}`
+      )
+    })
+  })
+  .catch(console.error)
