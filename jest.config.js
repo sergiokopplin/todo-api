5e3294b39aae5e2f7743dcb52f129f4ed7dc1ae4
Sergio Kopplin <sergiokopplin@gmail.com>
2021-03-18 18:26:15 -0300

    chore: 100% coverage

--- jest.config.js
+++ jest.config.js
@@ -16,5 +16,13 @@ module.exports = {
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'
+  },
+  coverageThreshold: {
+    global: {
+      "branches": 100,
+      "functions": 100,
+      "lines": 100,
+      "statements": 100
+    }
   }
 }
