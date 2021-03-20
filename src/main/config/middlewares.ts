a89e222dac151fb1ac71c3dd61b337979f5bb02c
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2021-03-20 18:20:09 -0300

    chore: adds logger middleware (#48)

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,17 @@
 import { Express } from 'express'
 
-import { bodyParser, cors, contentType, noCache } from '@/main/middlewares'
+import {
+  bodyParser,
+  cors,
+  contentType,
+  noCache,
+  logger
+} from '@/main/middlewares'
 
 export const middlewaresConfig = (app: Express): void => {
   app.use(contentType)
   app.use(bodyParser)
   app.use(cors)
   app.use(noCache)
+  app.use(logger)
 }
