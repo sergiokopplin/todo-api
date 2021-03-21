f8b024346c4372b2e29872c4cd1ea104e5cf7126
Sergio Kopplin <sergiokopplin@gmail.com>
2021-03-20 21:35:20 -0300

    chore: skip logger when test env

--- src/main/middlewares/logger.ts
+++ src/main/middlewares/logger.ts
@@ -1,3 +1,7 @@
 import morgan from 'morgan'
 
-export const logger = morgan('tiny')
+export const logger = morgan('tiny', {
+  skip: (_req, _res) => {
+    return process.env.NODE_ENV === 'test'
+  }
+})
