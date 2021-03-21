265d260ad7630f57f878f7c450a56c5824e05cb4
Sergio Kopplin <sergiokopplin@gmail.com>
2021-03-20 21:43:19 -0300

    feat: adds accountId on express route adapt

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -6,7 +6,8 @@ export const expressRouteAdapt = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
       ...(req.body || {}),
-      ...(req.params || {})
+      ...(req.params || {}),
+      accountId: req?.accountId
     }
     const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
